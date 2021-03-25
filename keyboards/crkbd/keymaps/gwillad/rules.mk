7df2b5a33a60f3def1c91423a5868771fdd8f354
Adam Gwilliam <agwilliam@yaharasoftware.com>
2021-03-25 09:12:35 -0500

    fix layout and add via

--- keyboards/crkbd/keymaps/gwillad/rules.mk
+++ keyboards/crkbd/keymaps/gwillad/rules.mk
@@ -1,3 +1,6 @@
 OLED_DRIVER_ENABLE= yes     # OLED display
 WPM_ENABLE = yes
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+MOUSEKEY_ENABLE = no
+VIA_ENABLE = yes
+LTO_ENABLE = yes
