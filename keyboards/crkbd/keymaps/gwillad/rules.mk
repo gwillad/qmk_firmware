bb757d7473abe636287e74b297a9337d50aadf64
Adam Gwilliam <agwilliam@yaharasoftware.com>
2021-03-28 08:47:00 -0500

    add keymap for crkbd

--- keyboards/crkbd/keymaps/gwillad/rules.mk
+++ keyboards/crkbd/keymaps/gwillad/rules.mk
@@ -1,4 +1,4 @@
-OLED_DRIVER_ENABLE= yes     # OLED display
+OLED_DRIVER_ENABLE = yes     # OLED display
 WPM_ENABLE = yes
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 MOUSEKEY_ENABLE = no
